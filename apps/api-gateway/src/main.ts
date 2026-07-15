dabb619a6e0cd7f0baf373644739b9c43ad2d3e6
Leonardo Rossi <83621860+leorossiio@users.noreply.github.com>
2026-07-14 21:31:02 -0300

    git commit -m "feat: setup api-gateway with reverse proxy routing"

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,48 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './api-gateway.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Roteamento para Médicos (Porta 3001)
+  app.use(
+    '/medicos',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+    }),
+  );
+
+  // Roteamento para Pacientes (Porta 3002)
+  app.use(
+    '/pacientes',
+    createProxyMiddleware({
+      target: 'http://localhost:3002', 
+      changeOrigin: true,
+    }),
+  );
+
+  // Roteamento para Atendimentos (Porta 3003)
+  app.use(
+    '/atendimentos',
+    createProxyMiddleware({
+      target: 'http://localhost:3003',
+      changeOrigin: true,
+    }),
+  );
+
+  // Roteamento para Auditoria (Porta 3004)
+  app.use(
+    '/auditoria',
+    createProxyMiddleware({
+      target: 'http://localhost:3004',
+      changeOrigin: true,
+    }),
+  );
+
+  // O API Gateway vai rodar na porta 4000
+  await app.listen(4000);
+  console.log('🚀 API Gateway rodando na porta 4000');
+}
+bootstrap();
